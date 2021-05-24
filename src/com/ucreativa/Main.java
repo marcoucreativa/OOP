2579b16b1732eadfb3142d236dc5b7a5f9c72fc8
Marco Vargas <marco.vargas@gorillalogic.com>
2021-05-24 05:38:38 -0600

    - Added new Marco.java class - Updated gitignore to avoid uploading IntelliJ files - Updated Main.java to add interaction with Marco.java

---
+++ src/com/ucreativa/Main.java
@@ -0,0 +1,11 @@
+package com.ucreativa;
+
+import com.ucreativa.familia.Marco;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Marco yo = new Marco();
+    }
+}
---
+++ src/com/ucreativa/familia/Marco.java
@@ -0,0 +1,10 @@
+package com.ucreativa.familia;
+
+public class Marco {
+
+    public Marco() {
+
+        System.out.println("Hola Clase");
+
+    }
+}
