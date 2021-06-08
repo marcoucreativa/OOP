24326a717fd41daea539b7a882bc5d787799bfdd
Marco Vargas <marco.vargas@gorillalogic.com>
2021-06-07 20:36:04 -0600

    Leccion #7 Trabajo en Clase #9

--- src/com/ucreativa/vacunacion/Main.java
+++ src/com/ucreativa/vacunacion/Main.java
@@ -1,20 +1,18 @@
 package com.ucreativa.vacunacion;
 
 import com.ucreativa.vacunacion.entities.Amigo;
-import com.ucreativa.vacunacion.entities.BitacoraVacunas;
 import com.ucreativa.vacunacion.entities.Familiar;
 import com.ucreativa.vacunacion.entities.Persona;
+import com.ucreativa.vacunacion.repositories.InMemoryRepository;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        List<BitacoraVacunas> db = new ArrayList<>();
+        InMemoryRepository repo = new InMemoryRepository();
         String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca, print;
         Persona persona;
         while (true) {
@@ -41,12 +39,12 @@ public class Main {
             }
             System.out.println("Vacuna -- Marca:");
             marca = in.nextLine();
-            db.add(new BitacoraVacunas(persona, marca, new Date()));
+            repo.save(persona, marca, new Date());
             System.out.println("Quiere imprimir Lista (S)");
             print = in.nextLine();
             if (print.equals("S")) {
-                for (BitacoraVacunas item : db) {
-                    System.out.println(item.getPersona().toString() + " vacunado el " + item.getFecha());
+                for (String item : repo.get()) {
+                    System.out.println(item);
                 }
             }
         } // While
